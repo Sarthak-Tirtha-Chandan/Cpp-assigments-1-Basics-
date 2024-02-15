cb9f891f5c84089420a1a66568938220cffc324d
Sarthak-Tirtha-Chandan <159796103+Sarthak-Tirtha-Chandan@users.noreply.github.com>
2024-02-15 20:29:32 +0530

    solved assignment files are added

---
+++ Assignment 1 solution (basics)/Answer_to_Q5.cpp
@@ -0,0 +1,12 @@
+//Q5.Program to find the circumference of a circle with radius 10 in C++
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int radius = 10;
+    float  circumference;
+    circumference = 2*3.1415*radius;
+    cout<<"The circumference of the circle is:"<<circumference;
+    return 0;
+}
